db2411d6f9eee94975421bc15a5f7c3ebc1b6990
Dan <dan.kirichok@gmail.com>
2017-06-03 14:41:32 -0400

    Looks like new server configuration set up works, so deleted old server set up.

--- server.js
+++ server.js
@@ -301,20 +301,17 @@ io.on('connection', function(socket){
 	})
 })
 
-//This is for openshift deployment
+//Server configuration stuff
 
+//Tries to find variable for openshift ip, if nothing then loads to localhost
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
+//Same as above, but with port
+//If you want to load on localhost onto a different port, change 4000 to whatever port you please
 var port = process.env.OPENSHIFT_NODEJS_PORT || 4000;
 
 http.listen(port, ipaddress, function(){
 	console.log('Running on Openshift Server')
 })
 
-/*
-//This is for testing
-http.listen(4000, function(){
-	console.log('listening on *:4000')
-})
-*/
 
